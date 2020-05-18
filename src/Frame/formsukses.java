013823a8a0b4cb9fa007d5f0e2772adb893f1b00
aldipubiangga <55964234+aldipubiangga@users.noreply.github.com>
2020-05-19 02:37:09 +0700

    tubes pbo final

--- src/Frame/formsukses.java
+++ src/Frame/formsukses.java
@@ -31,7 +31,9 @@ public class formsukses extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -45,25 +47,37 @@ public class formsukses extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButton1);
-        jButton1.setBounds(200, 260, 47, 23);
+        jButton1.setBounds(220, 250, 47, 20);
 
         jLabel2.setFont(new java.awt.Font("Humnst777 Cn BT", 1, 14)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("Data sudah tersimpan silahkan memverifikasinya dikelurahan terdekat");
         jPanel1.add(jLabel2);
-        jLabel2.setBounds(20, 100, 420, 80);
+        jLabel2.setBounds(20, 90, 420, 80);
 
         jLabel3.setFont(new java.awt.Font("Humnst777 BT", 1, 11)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Note : Silahkan membawa data diri seperti KTP/KK");
         jPanel1.add(jLabel3);
-        jLabel3.setBounds(70, 180, 300, 50);
+        jLabel3.setBounds(90, 170, 300, 50);
 
-        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\ACER\\Pictures\\268445_wallpaper-polos-keren.jpg")); // NOI18N
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frame/keluar.png"))); // NOI18N
+        jPanel1.add(jLabel4);
+        jLabel4.setBounds(410, 10, 30, 30);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Frame/guiback.jpeg"))); // NOI18N
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(-10, 0, 450, 340);
+        jLabel1.setBounds(-10, 0, 460, 318);
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 440, 340));
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jButton2);
+        jButton2.setBounds(410, 0, 40, 40);
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 450, 320));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -72,6 +86,10 @@ public class formsukses extends javax.swing.JFrame {
         this.setVisible(false);
         new formawal().setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        this.dispose();
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -110,9 +128,11 @@ public class formsukses extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
